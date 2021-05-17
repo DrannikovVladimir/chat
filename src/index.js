2014672f8c8990aba597e14cd96b129d2d3b780f
Vladimir Drannikov <krowen@inbox.ru>
2021-05-17 09:04:14 +0600

    Imported socket

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import 'core-js/stable/index.js';
 import 'regenerator-runtime/runtime.js';
 
 import '../assets/application.scss';
+import { io } from 'socket.io-client';
 // @ts-ignore
 import run from './index.jsx';
 
@@ -11,4 +12,6 @@ if (process.env.NODE_ENV !== 'production') {
   localStorage.debug = 'chat:*';
 }
 
-run();
+const socket = io();
+
+run(socket);
--- src/index.jsx
+++ src/index.jsx
@@ -2,15 +2,26 @@ import React from 'react';
 import { render } from 'react-dom';
 import { Provider } from 'react-redux';
 import store from './app/store.js';
+import { SocketContext } from './contexts/index.jsx';
+import { addMessage } from './slices/messagesSlice.js';
 
 import App from './components/App.jsx';
 
-const run = () => {
-  console.log('run');
+const SocketProvider = ({ children, socket }) => (
+  <SocketContext.Provider value={socket}>
+    {children}
+  </SocketContext.Provider>
+);
+
+const run = (socket) => {
+  socket.on('newMessage', (message) => store.dispatch(addMessage({ message })));
+
   return render(
-    <Provider store={store}>
-      <App />
-    </Provider>,
+    <SocketProvider socket={socket}>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </SocketProvider>,
     document.querySelector('#chat'),
   );
 };
