0dfbe713e2b66172f20ab2291780ac0567781e3c
Vladimir Drannikov <krowen@inbox.ru>
2021-06-17 14:20:09 +0600

    Hided token

--- src/contexts/index.jsx
+++ src/contexts/index.jsx
@@ -2,7 +2,7 @@ import { createContext } from 'react';
 import Rollbar from 'rollbar';
 
 const rollbar = new Rollbar({
-  accessToken: '7139cb6f908947c2b6166d41e28fe1a1',
+  accessToken: process.env.TOKEN,
   captureUncaught: true,
   captureUnhandledRejections: true,
 });
